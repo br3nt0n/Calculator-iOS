0933b4b05da19b7f27b56da8fbaf00727669132f
Brenton <brenton.currie@auspost.com.au>
2015-11-17 11:04:29 +1100

    End of Lecure 1

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,16 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingNumber: Bool = false
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func appendDigit(sender: UIButton) {
+        //Optional type has two values - nil (Not set), or something
+        //An optional that can be a string
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingNumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingNumber = true
+        }
+        
+        print("digit = \(digit)")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
