02143fcc7f5fe970c18cbc740da325ec031c467f
Brenton <brenton.currie@auspost.com.au>
2015-11-18 13:48:33 +1100

    Midway through lecture 2

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -18,5 +18,61 @@ class ViewController: UIViewController {
         
         print("digit = \(digit)")
     }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTypingNumber {
+            enter()
+        }
+        
+        switch operation {
+        case "×": performCalculation {$0 * $1}
+            
+        case "÷": performCalculation {$1 / $0}
+            
+        case "+": performCalculation {$0 + $1}
+            
+        case "−": performCalculation {$1 - $0}
+            
+        case "√": performCalculation {sqrt($0)}
+            
+        default: break
+        }
+    }
+    
+    //Operation is a type
+    func performCalculation(operation: (Double, Double) -> Double){
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    @nonobjc func performCalculation(operation: Double -> Double){
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+
+    
+    var operandStack: Array<Double> = Array<Double>()
+
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingNumber = false
+        operandStack.append(displayValue)
+        print("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingNumber = false
+        }
+    }
 }
 
